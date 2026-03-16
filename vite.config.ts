554fe1cbae27920ea319c80309e636416463c72e
heinthantm99-cmyk <heinthantm99@gmail.com>
2026-03-16 16:26:26 +0700

    language error fix for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite";
 import vueI18n from '@intlify/unplugin-vue-i18n/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueI18n({
-      include: './src/locales/**',   // ← This is the important line
-    }),tailwindcss()],
+  plugins: [vue(),
+     vueI18n({
+      include: path.resolve(__dirname, './src/locales/**'),   // ← This is the important line
+    }),
+    tailwindcss()],
 })
