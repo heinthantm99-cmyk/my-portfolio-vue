b3bfa7f8dce21de027e9fc77606b197e36d650db
heinthantm99-cmyk <heinthantm99@gmail.com>
2026-03-16 16:54:51 +0700

    added new library

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite";
 import vueI18n from '@intlify/unplugin-vue-i18n/vite'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(),
      vueI18n({
-      include: path.resolve(__dirname, './src/locales/**'),   // ← This is the important line
-      strictMessage: false,
+     runtimeOnly: false,
     }),
     tailwindcss()],
     resolve: {
     alias: {
+      // Production မှာ i18n အလုပ်လုပ်စေရန် alias ထည့်ပေးပါ
       'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-browser.js'
     }
   }
